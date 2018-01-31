5ac45b6e9b908d649ae50d498dc4cd7317fa6521
Kostya S <kostya.s@smartum.pro>
2018-01-31 19:20:56 +0300

    Added babel to project. Added tests for ZapRegistry. Starting test for ZapBondage. Some refactoring for ZapDispatchExample.

--- migrations/7_zap_dispatch_example_migration.js
+++ migrations/7_zap_dispatch_example_migration.js
@@ -2,8 +2,9 @@ var ZapDispatchExample = artifacts.require("./ZapDispatchExample.sol");
 var ZapDispatch = artifacts.require("./ZapDispatch.sol");
 var ZapBondage = artifacts.require("./ZapBondage.sol");
 var ZapToken = artifacts.require("./ZapToken.sol");
+var ZapRegistry = artifacts.require("./ZapRegistry.sol");
 
 
 module.exports = function(deployer) {
-    deployer.deploy(ZapDispatchExample, ZapToken.address, ZapDispatch.address, ZapBondage.address);
+    deployer.deploy(ZapDispatchExample, ZapToken.address, ZapDispatch.address, ZapBondage.address, ZapRegistry.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
 };
