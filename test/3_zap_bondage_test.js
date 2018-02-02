4514605f2dd5c23c3c212d639030cc98ad349ed2
Kostya S <kostya.s@smartum.pro>
2018-02-02 18:52:01 +0300

    Added utils file to helpers. Bond test for ZapBondage.

--- test/3_zap_bondage_test.js
+++ test/3_zap_bondage_test.js
@@ -4,6 +4,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .use(require('chai-bignumber')(BigNumber))
     .should();
+require('./helpers/utils');
 
 const ZapRegistry = artifacts.require("ZapRegistry");
 const ZapToken = artifacts.require("ZapToken");
@@ -23,18 +24,42 @@ const deployZapBondage = (tokenAddress, registryAddress) => {
     return ZapBondage.new(tokenAddress, registryAddress);
 };
 
+const curveTypes = {
+    "None": 0,
+    "Linier": 1,
+    "Exponentioal": 2,
+    "Logarithmic": 3
+}
+
 contract('ZapBondage', function (accounts) {
     const owner = accounts[0];
+    const provider = account[1];
 
-    it("ZAP_BONDAGE_1 - Check that we can initiate provider", async function () {
+    it("ZAP_BONDAGE_1 - Check bond function", async function () {
         let zapRegistry = await deployZapRegistry();
         let zapToken = await deployZapToken();
         let zapBondage = await deployZapBondage(zapToken.address, zapRegistry.address);
 
 
         const publicKey = 111;
-        const title = "test"; 
+        const title = "test";
+        const routeKeys = [1]; 
 
-        await zapRegistry.initiateProvider(publicKey, [1], title, { from: owner });
+        const specifier = new String("test-liner-specifier");
+        const curve = curveTypes["Linier"];
+        const start = 1;
+        const mul = 2;
+
+        await zapRegistry.initiateProvider(publicKey, routeKeys, title, { from: owner });
+        await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: owner });
+
+
+        await zapToken.allocate(owner, 1500 * DECIMALS, { from: owner });
+        await zapToken.allocate(provider, 5000 * DECIMALS, { from: owner });
+        await zapToken.approve(zapBondage.address, 1000 * DECIMALS, {from: provider});
+
+        const res = await zapBondage.bond(specifier.valueOf(), 1000, owner, {from: owner});
+        console.log(res)
+
     });
 });
