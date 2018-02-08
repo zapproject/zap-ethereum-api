d2df1b120485d1d0b41b8879c5a8aac69e6165e8
Kostya S <kostya.s@smartum.pro>
2018-02-08 15:46:06 +0300

    Removed todo's, test fixes for ZapBondage.

--- test/3_zap_bondage_test.js
+++ test/3_zap_bondage_test.js
@@ -60,41 +60,14 @@ contract('ZapBondage', function (accounts) {
         await zapRegistry.initiateProvider(publicKey, routeKeys, title, { from: provider });
         await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: provider });
 
-
         await zapToken.allocate(owner, 1500 * DECIMALS, { from: owner });
         await zapToken.allocate(provider, 5000 * DECIMALS, { from: owner });
         await zapToken.approve(zapBondage.address, 1000 * DECIMALS, {from: provider});
 
-        const res = await zapBondage.bond(specifier.valueOf(), 1000, oracle.address, {from: provider});
+        const res = await zapBondage.bond(specifier.valueOf(), 100, provider, {from: provider});
     });
 
-    it("ZAP_BONDAGE_2 - Check that we can't bond oracle with unregistered provider", async function () {
-        let zapRegistry = await deployZapRegistry();
-        let zapToken = await deployZapToken();
-        let zapBondage = await deployZapBondage(zapToken.address, zapRegistry.address);
-        let oracle = await deployTestOracle();
-
-        const publicKey = 111;
-        const title = "test";
-        const routeKeys = [1]; 
-
-        const specifier = new String("test-liner-specifier");
-        const curve = curveTypes["Linier"];
-        const start = 1;
-        const mul = 2;
-
-        //await zapRegistry.initiateProvider(publicKey, routeKeys, title, { from: provider });
-        await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: provider });
-
-
-        await zapToken.allocate(owner, 1500 * DECIMALS, { from: owner });
-        await zapToken.allocate(provider, 5000 * DECIMALS, { from: owner });
-        await zapToken.approve(zapBondage.address, 1000 * DECIMALS, {from: provider});
-
-        zapBondage.bond(specifier.valueOf(), 1000, oracle.address, {from: provider}).should.be.eventually.rejectedWith(EVMRevert);
-    });
-
-    it("ZAP_BONDAGE_3 - Check that we can't bond oracle with uninitialized curve", async function () {
+    it("ZAP_BONDAGE_2 - Check that bond will not performed if provider was not approve tokens", async function () {
         let zapRegistry = await deployZapRegistry();
         let zapToken = await deployZapToken();
         let zapBondage = await deployZapBondage(zapToken.address, zapRegistry.address);
@@ -110,8 +83,37 @@ contract('ZapBondage', function (accounts) {
         const mul = 2;
 
         await zapRegistry.initiateProvider(publicKey, routeKeys, title, { from: provider });
-        //await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: provider });
+        await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: provider });
 
+        let dotsAndZap = await zapBondage.calcZap.call(provider, specifier.valueOf(), 100, {from: provider});
+        console.log("dots count for zap tokens: ", dotsAndZap[1].valueOf());
+        console.log("number of zap for bonding: ", dotsAndZap[0].valueOf());
+
+
+        //await zapToken.allocate(owner, 1500 * DECIMALS, { from: owner });
+        //await zapToken.allocate(provider, 5000 * DECIMALS, { from: owner });
+        //await zapToken.approve(zapBondage.address, 1000 * DECIMALS, {from: provider});
+
+        //zapBondage.bond(specifier.valueOf(), 1000, oracle.address, {from: provider}).should.be.eventually.rejectedWith(EVMRevert);
+    });
+
+    it("ZAP_BONDAGE_3 - Check that bond will not performed if provider or curve is not initialzied", async function () {
+        let zapRegistry = await deployZapRegistry();
+        let zapToken = await deployZapToken();
+        let zapBondage = await deployZapBondage(zapToken.address, zapRegistry.address);
+        let oracle = await deployTestOracle();
+
+        const publicKey = 111;
+        const title = "test";
+        const routeKeys = [1]; 
+
+        const specifier = new String("test-liner-specifier");
+        const curve = curveTypes["Linier"];
+        const start = 1;
+        const mul = 2;
+
+        //await zapRegistry.initiateProvider(publicKey, routeKeys, title, { from: provider });
+        //await zapRegistry.initiateProviderCurve(specifier.valueOf(), curve, start, mul, { from: provider });
 
         await zapToken.allocate(owner, 1500 * DECIMALS, { from: owner });
         await zapToken.allocate(provider, 5000 * DECIMALS, { from: owner });
