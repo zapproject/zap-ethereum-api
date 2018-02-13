585923f7e39c7267fb3a9cb4b3f7b5ca5a141e40
Kostya S <kostya.s@smartum.pro>
2018-02-13 11:36:48 +0300

    ZapBondage tests refactoring.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,4 +1,13 @@
-export function fetchPureArray(res, parseFunc) {
+module.exports.ZeroAddress = "0x0000000000000000000000000000000000000000";
+
+module.exports.CurveTypes = {
+    "None": 0,
+    "Linier": 1,
+    "Exponential": 2,
+    "Logarithmic": 3
+};
+
+module.exports.fetchPureArray = function (res, parseFunc) {
     let arr = [];
     for (let key in res) {
         arr.push(parseFunc(res[key].valueOf()));
@@ -6,10 +15,38 @@ export function fetchPureArray(res, parseFunc) {
     return arr;
 }
 
-export function calculateZapWithLinierCurve(dotsRequired, startValue, multiplier) {
+module.exports.calculateZapWithLinierCurve = function (dotsRequired, startValue, multiplier) {
     let zap = 0;
     for (i = 0; i < dotsRequired; i++) {
         zap += multiplier * i + startValue
     }
     return zap;
+}
+
+module.exports.calculateZapWithLinearCurve = function (dotsRequired, startValue, multiplier) {
+    let zap = 0;
+    for (let i = 0; i < dotsRequired; i++) {
+        zap += multiplier * i + startValue
+    }
+    return zap;
+}
+
+module.exports.calculateZapWithExponentialCurve = function (dotsRequired, startValue, multiplier) {
+    let zap = 0;
+    for (let i = 0; i < dotsRequired; i++) {
+        zap += multiplier * Math.pow(i, 2) + startValue;
+    }
+    return zap;
+}
+
+module.exports.calculateZapWithLogarithmicCurve = function (dotsRequired, startValue, multiplier) {
+    let zap = 0;
+    for (let i = 0; i < dotsRequired; i++) {
+        let totalBound = i;
+        if (totalBound == 0) {
+            totalBound = 1;
+        }
+        zap += multiplier * Math.log2(totalBound) + startValue;
+    }
+    return Math.ceil(zap);
 }
