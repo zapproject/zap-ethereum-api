31873d0318e2703515af2dbd69d811207bbb8c07
Dax Bondye <beckwithdylan@gmail.com>
2018-03-17 18:18:15 -0700

    made all interface fxs external

--- contracts/bondage/BondageInterface.sol
+++ contracts/bondage/BondageInterface.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.4.17;
 // Make sure to implement a setBondageAddress fx in contracts that use this interface to support upgrades
 
 interface BondageInterface {
-    function bond(address, bytes32, uint256) public returns(uint256);
-    function unbond(address, bytes32, uint256) public returns (uint256);
-    function escrowDots(address, address, bytes32, uint256) public returns (bool);
-    function releaseDots(address, address, bytes32, uint256) public returns (bool);
-    function calcTokForDots(address, bytes32, uint256) public view returns (uint256);
-    function calcTok(address, bytes32, uint256) public view returns (uint256, uint256);
-    function currentCostOfDot(address, bytes32, uint256) public view returns (uint256);
-    function getDotsIssued(address, bytes32) public view returns (uint256);
-    function getDots(address, address, bytes32) public view returns (uint256);
-    function getTokBound(address, bytes32) public view returns (uint256);
+    function bond(address, bytes32, uint256) external returns(uint256);
+    function unbond(address, bytes32, uint256) external returns (uint256);
+    function escrowDots(address, address, bytes32, uint256) external returns (bool);
+    function releaseDots(address, address, bytes32, uint256) external returns (bool);
+    function calcTokForDots(address, bytes32, uint256) external view returns (uint256);
+    function calcTok(address, bytes32, uint256) external view returns (uint256, uint256);
+    function currentCostOfDot(address, bytes32, uint256) external view returns (uint256);
+    function getDotsIssued(address, bytes32) external view returns (uint256);
+    function getDots(address, address, bytes32) external view returns (uint256);
+    function getTokBound(address, bytes32) external view returns (uint256);
 }
--- contracts/registry/RegistryInterface.sol
+++ contracts/registry/RegistryInterface.sol
@@ -6,12 +6,12 @@ pragma solidity ^0.4.17;
 
 contract RegistryInterface {
 	enum CurveType { None, Linear, Exponential, Logarithmic }
-    function initiateProvider(uint256, string, bytes32, bytes32[]) public returns (bool);
-    function initiateProviderCurve(bytes32, CurveType, uint128, uint128) public;
-    function setEndpointParams(bytes32, bytes32[]) public;
-    function getProviderPublicKey(address) public view returns (uint256);
-    function getProviderTitle(address) public view returns (string);
-	function getNextRouteKey(address, bytes32, uint256) public view returns (uint256, bytes32);
-    function getProviderCurve(address, bytes32) public view returns (CurveType, uint128, uint128);
-    function getNextProvider(uint256) public view returns (uint256, address, uint256, string);
+    function initiateProvider(uint256, string, bytes32, bytes32[]) external returns (bool);
+    function initiateProviderCurve(bytes32, CurveType, uint128, uint128) external;
+    function setEndpointParams(bytes32, bytes32[]) external;
+    function getProviderexternalKey(address) external view returns (uint256);
+    function getProviderTitle(address) external view returns (string);
+	function getNextRouteKey(address, bytes32, uint256) external view returns (uint256, bytes32);
+    function getProviderCurve(address, bytes32) external view returns (CurveType, uint128, uint128);
+    function getNextProvider(uint256) external view returns (uint256, address, uint256, string);
 }
