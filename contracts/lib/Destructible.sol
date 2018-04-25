787ed8ed35705e02aa77c7b5cd45de8aa4a745d3
Dax Bondye <beckwithdylan@gmail.com>
2018-04-25 11:08:28 -0700

    implode => selfDestruct

--- contracts/lib/Destructible.sol
+++ contracts/lib/Destructible.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "./Ownable.sol";
 
 contract Destructible is Ownable {
-	function implode() public onlyOwner {
+	function selfDestruct() public onlyOwner {
 		selfdestruct(owner);
 	}
 }
