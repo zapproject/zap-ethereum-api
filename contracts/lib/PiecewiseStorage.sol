ca054c4755ac96b628d38d8d1a0903e731fb9d7a
kim <propulsor.ichimoku@gmail.com>
2018-05-31 17:23:03 -0600

    - Changed names of args in PiecewiseLogic to match the structs - fixed small bugs return val*term.coef instead of exp - constants  hop (coef,power,fn); parts hop (start, end)

--- contracts/lib/PiecewiseStorage.sol
+++ contracts/lib/PiecewiseStorage.sol
@@ -21,26 +21,20 @@ library PiecewiseStorage {
                          uint[] dividers,
                          PiecewiseStorage.PiecewisePiece[] storage out) internal {
         uint pStart = 0;
-        uint pieceLength = out.length;
-        // out.pieces = new PiecewiseStorage.PiecewisePiece[](dividers.length + 1);
 
         for ( uint i = 0; i < dividers.length; i++ ) {
-            // PiecewiseStorage.PiecewiseTerm[] memory terms = new PiecewiseStorage.PiecewiseTerm[](10);
 
             out[i].start = parts[2 * i];
             out[i].end = parts[(2 * i) + 1];
 
             for ( uint j = pStart; j < dividers[i]; j++ ) {
-                out[i].terms[j - dividers[i]].coef  = constants[(pieceLength * j) + 0];
-                out[i].terms[j - dividers[i]].power = constants[(pieceLength * j) + 1];
-                out[i].terms[j - dividers[i]].fn    = constants[(pieceLength * j) + 2];
+                out[i].terms[j - pStart].coef  = constants[(3 * j) + 0];
+                out[i].terms[j - pStart].power = constants[(3 * j) + 1];
+                out[i].terms[j - pStart].fn    = constants[(3 * j) + 2];
             }
 
             pStart = dividers[i];
         }
 
-        // PiecewiseStorage.PiecewiseFunction memory out = PiecewiseStorage.PiecewiseFunction(pieces);
-
-        // return pieces;
     }
 }
