7a1b7f631ee549459ab1c80ae1f902c1784b8494
Michael Man <17michaelm@gmail.com>
2018-08-07 17:39:31 -0400

    Added bondagestorage changes

--- contracts/platform/bondage/BondageStorage.sol
+++ contracts/platform/bondage/BondageStorage.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.4.24;
 
 import "../../lib/ownership/Ownable.sol";
+import "../database/DatabaseInterface.sol";
 
 contract BondageStorage is Ownable {
 
+    DatabaseInterface public db;
+
+    constructor(address database) public {
+        db = DatabaseInterface(database);
+    }
+/*
     // Data structure for holder of ZAP bond to data provider.
     // Currently ONLY "smart_contract" or "socket_subscription"
     struct Holder {
@@ -24,113 +31,101 @@ contract BondageStorage is Ownable {
     mapping(address => mapping(bytes32 => uint256)) private totalBound;
 
     //oracleAddress=>(endpoint=>numDots)
-    mapping(address => mapping(bytes32 => uint256)) private totalIssued;
+    mapping(address => mapping(bytes32 => uint256)) private totalIssued;*/
 
     /**** Get Methods ****/
     function isProviderInitialized(address holderAddress, address oracleAddress) external view returns (bool) {
-        return holders[holderAddress].initialized[oracleAddress];
-    }
-
-    function getBondValue(address holderAddress, address oracleAddress, bytes32 endpoint) external view returns (uint256) {
-        return holders[holderAddress].bonds[endpoint][oracleAddress];
+        return db.getNumber(keccak256(abi.encodePacked('holders', holderAddress, 'initialized', oracleAddress))) == 1 ? true : false;
     }
 
     function getNumEscrow(address holderAddress, address oracleAddress, bytes32 endpoint) external view returns (uint256) {
-        return pendingEscrow[holderAddress][oracleAddress][endpoint];
+        return uint(db.getNumber(keccak256(abi.encodePacked('escrow', holderAddress, oracleAddress, endpoint))));
+        //return pendingEscrow[holderAddress][oracleAddress][endpoint];
     }
 
     function getNumZap(address oracleAddress, bytes32 endpoint) external view returns (uint256) {
-        return totalBound[oracleAddress][endpoint];
+        return uint(db.getNumber(keccak256(abi.encodePacked('totalBound', oracleAddress, endpoint))));
+        //return totalBound[oracleAddress][endpoint];
     }
 
     function getDotsIssued(address oracleAddress, bytes32 endpoint) external view returns (uint256) {
-        return totalIssued[oracleAddress][endpoint];
+        return uint(db.getNumber(keccak256(abi.encodePacked('totalIssued', oracleAddress, endpoint))));
+        //return totalIssued[oracleAddress][endpoint];
     }
 
     function getBoundDots(address holderAddress, address oracleAddress, bytes32 endpoint) external view returns (uint256) {
-        return holders[holderAddress].bonds[endpoint][oracleAddress];
+        return uint(db.getNumber(keccak256(abi.encodePacked('holders', holderAddress, 'bonds', oracleAddress, endpoint))));
+        //return holders[holderAddress].bonds[endpoint][oracleAddress];
     }
 
     function getIndexSize(address holderAddress) external view returns (uint256) {
-        return holders[holderAddress].oracleList.length;
+        return db.getAddressArrayLength(keccak256(abi.encodePacked('holders', holderAddress, 'oracleList')));
     }
 
     function getOracleAddress(address holderAddress, uint256 index) external view returns (address) {
-        return holders[holderAddress].oracleList[index];
+        return db.getAddressArrayIndex(keccak256(abi.encodePacked('holders', holderAddress, 'oracleList')), index);
+        //return holders[holderAddress].oracleList[index];
     }
 
     /**** Set Methods ****/
     function addHolderOracle(address holderAddress, address oracleAddress) external onlyOwner {
-        holders[holderAddress].oracleList.push(oracleAddress);
+        db.pushAddressArray(keccak256(abi.encodePacked('holders', holderAddress, 'oracleList')), holderAddress);
+        //holders[holderAddress].oracleList.push(oracleAddress);
     }
 
     function setProviderInitialized(address holderAddress, address oracleAddress) external onlyOwner {
-        holders[holderAddress].initialized[oracleAddress] = true;
+        db.setNumber(keccak256(abi.encodePacked('holders', holderAddress, 'initialized', oracleAddress)), 1);
+        //holders[holderAddress].initialized[oracleAddress] = true;
     }
 
-    function updateEscrow(
-        address holderAddress,
-        address oracleAddress,
-        bytes32 endpoint,
-        uint256 numDots,
-        bytes32 op
-    )
-        external
-        onlyOwner      
+    function updateEscrow(address holderAddress, address oracleAddress, bytes32 endpoint, uint256 numDots, bytes32 op)
+    external onlyOwner      
     {
-        if (op == "sub")
-            pendingEscrow[holderAddress][oracleAddress][endpoint] -= numDots;
-        else {
-            pendingEscrow[holderAddress][oracleAddress][endpoint] += numDots;
+        uint256 newEscrow = this.getNumEscrow(holderAddress, oracleAddress, endpoint);
+        if (op == "sub"){
+            newEscrow -= numDots;
+             //pendingEscrow[holderAddress][oracleAddress][endpoint] -= numDots;
+        } else {
+            newEscrow += numDots;
+          //  pendingEscrow[holderAddress][oracleAddress][endpoint] += numDots;
         }
+
+        db.setNumber(keccak256(abi.encodePacked('escrow', holderAddress, oracleAddress, endpoint)), newEscrow);
     }
 
-    function updateBondValue(
-        address holderAddress,
-        address oracleAddress,
-        bytes32 endpoint,
-        uint256 numDots,
-        bytes32 op
-    )
-        external
-        onlyOwner      
+    function updateBondValue(address holderAddress, address oracleAddress, bytes32 endpoint, uint256 numDots, bytes32 op)
+    external onlyOwner      
     {
-        if (op == "sub") 
-            holders[holderAddress].bonds[endpoint][oracleAddress] -= numDots;
-        else {
-            holders[holderAddress].bonds[endpoint][oracleAddress] += numDots;
+        uint256 bondValue = this.getBoundDots(holderAddress, oracleAddress, endpoint);
+        if (op == "sub") {
+            bondValue -= numDots;
+        }else {
+            bondValue += numDots;
         }
+        db.setNumber(keccak256(abi.encodePacked('holders', holderAddress, 'bonds', oracleAddress, endpoint)), numDots);
     }
 
-    function updateTotalBound(        
-        address oracleAddress,
-        bytes32 endpoint,
-        uint256 numZap,
-        bytes32 op
-    )
-        external
-        onlyOwner       
+    function updateTotalBound(address oracleAddress, bytes32 endpoint, uint256 numZap, bytes32 op)
+    external onlyOwner       
     {
-        if (op == "sub")
-            totalBound[oracleAddress][endpoint] -= numZap;
-        else {
-            totalBound[oracleAddress][endpoint] += numZap;
+        uint256 totalBound = uint(db.getNumber(keccak256(abi.encodePacked('totalBound', oracleAddress, endpoint))));
+        if (op == "sub"){
+            totalBound -= numZap;
+        }else {
+            totalBound += numZap;
         }
+        db.setNumber(keccak256(abi.encodePacked('totalBound', oracleAddress, endpoint)), totalBound);
     }
 
-    function updateTotalIssued(
-        address oracleAddress,
-        bytes32 endpoint,
-        uint256 numDots,
-        bytes32 op
-    )
-        external
-        onlyOwner      
+    function updateTotalIssued(address oracleAddress, bytes32 endpoint, uint256 numDots, bytes32 op)
+    external
+    onlyOwner      
     {
-        if (op == "sub")
-            totalIssued[oracleAddress][endpoint] -= numDots;
-        else {
-            totalIssued[oracleAddress][endpoint] += numDots;
+        uint256 totalIssued = this.getDotsIssued(oracleAddress, endpoint);
+        if (op == "sub"){
+            totalIssued -= numDots;
+        } else {
+            totalIssued += numDots;
         }
     }
 
