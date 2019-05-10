8bb3d5da01c9914721e0493e33e608950972c40c
propulsor <propulsor.ichimoku@gmail.com>
2019-05-09 18:24:04 -0600

    fixed some small errors

--- contracts/lib/platform/BTCcontest.sol
+++ contracts/lib/platform/BTCcontest.sol
@@ -4,14 +4,34 @@ import "../../platform/dispatch/DispatchInterface.sol";
 import "./Client.sol";
 
 
+/// @title BTCcontest
+/// @notice
+/// @dev
 contract BTCcontest is Ownable, ClientIntArray {
+  /// @notice
   SampleContest public contest;
+  /// @notice
   ZapCoordinatorInterface public coordinator;
+  /// @notice
   address public oracle;
+  /// @notice
   address public contest;
+  /// @notice
   uint256 public query_id;
-  uint256 settlePrice;
+  /// @notice
+  uint256 public settlePrice;
+  /// @notice
+  bytes32 public upEndpoint;
+  /// @notice
+  bytes32 public downEndpoint;
 
+  /// @notice
+  /// @dev
+  /// @param _contest
+  /// @param _settlePrice
+  /// @param _upEndpoint
+  /// @param _downEndpoint
+  /// @return
   constructor(
     address _contest,
     uint256 _settlePrice,
@@ -21,21 +41,32 @@ contract BTCcontest is Ownable, ClientIntArray {
     oracle = msg.sender;
     contest = SampleContest(_contest);
     settlePrice = _settlePrice;
-    require(contest.isEndpointValid(_upEndpoint) && contest.isEndpointValid(_downEndpoint),"Endpoints are not valid")
+    require(contest.isEndpointValid(_upEndpoint) && contest.isEndpointValid(_downEndpoint),"Endpoints are not valid");
     upEndpoint = _upEndpoint;
     downEndpoint = _downEndpoint;
   }
 
+  /// @notice
+  /// @dev
+  /// @param _coincap
+  /// @param _endpoint
+  /// @return
   function queryToSettle(address _coincap,bytes32 _endpoint) public returns(uint256){
     require(msg.sender == oracle, "Only Oracle owner can call query to settle");
     address dispatchAddress = coordinator.getContract("DISPATCH");
     DispatchInterface dispatch = DispatchInterface(dispatchAddress);
-    query_id = dispatch.query(_coincap,"BTC",_endpoint);
+    bytes32[] memory params = bytes32[];
+    query_id = dispatch.query(_coincap,"BTC",_endpoint,params);
     return query_id;
   }
 
 
 
+  /// @notice
+  /// @dev
+  /// @param _id
+  /// @param responses
+  /// @return
   function callback(uint256 _id, int[] responses) external {
     address dispatchAddress = coordinator.getContract("DISPATCH");
     require(_id == query_id,"Query id is not correct");
@@ -44,10 +75,10 @@ contract BTCcontest is Ownable, ClientIntArray {
     uint256 price = uint256(responses[0]);
     bytes32[] endpoints = contest.getEndpoints();
     for(uint256 i=0;i<endpoints.length;i++){
-      if(endpoints[i]==up_endpoint && price > settlePrice){
+      if(endpoints[i]==upEndpoint && price > settlePrice){
         return contest.settle(endpoints[i]);
       }
-      if(endpoints[i]==down_endpoint && price<settlePrice){
+      if(endpoints[i]==downEndpoint && price<settlePrice){
         return contest.settle(endpoints[i]);
       }
     }
