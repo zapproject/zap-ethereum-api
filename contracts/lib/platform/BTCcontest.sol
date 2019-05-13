c430cf71ac92a4948b2f7a1c408ba560a931d755
propulsor <propulsor.ichimoku@gmail.com>
2019-05-13 13:54:10 -0600

    - Added test for BTCcontest - BTCcontest contract has to bond to coincap to be able to query

--- contracts/lib/platform/BTCcontest.sol
+++ contracts/lib/platform/BTCcontest.sol
@@ -10,25 +10,36 @@ contract BTCcontest is Ownable, ClientIntArray {
   ZapCoordinatorInterface public coordinator;
   address public oracle;
   uint256 public query_id;
-  uint256 public settlePrice;
+  uint256 public startPrice;
   bytes32 public upEndpoint;
   bytes32 public downEndpoint;
 
 
   constructor(
     address _contest,
-    uint256 _settlePrice,
+    uint256 _startPrice,
     bytes32 _upEndpoint,
     bytes32 _downEndpoint
   ){
     oracle = msg.sender;
     contest = SampleContest(_contest);
-    settlePrice = _settlePrice;
+    startPrice = _startPrice;
     require(contest.isEndpointValid(_upEndpoint) && contest.isEndpointValid(_downEndpoint),"Endpoints are not valid");
     upEndpoint = _upEndpoint;
     downEndpoint = _downEndpoint;
   }
 
+  function bondToCoincap(address _coincap,bytes32 _endpoint,uint256 _numDots)public returns (bool){
+    address bondageAddress = coordinator.getContract("BONDAGE");
+    BondageInterface bondage = BondageInterface(bondageAddress);
+    urrentCost = CurrentCostInterface(coord.getContract("CURRENT_COST"));
+    //get reserve value to send
+    uint issued = bondage.getDotsIssued(address(this), endpoint);
+    uint reserveCost = currentCost._costOfNDots(address(this), endpoint, issued + 1 - _numDots, _numDots - 1);
+    resevetoken.approve(address(bondageAddress),reserveCost);
+    bondage.bond(_coincap,_endpoint,_numDots);
+
+  }
   function queryToSettle(address _coincap,bytes32 _endpoint) public returns(uint256){
     require(msg.sender == oracle, "Only Oracle owner can call query to settle");
     address dispatchAddress = coordinator.getContract("DISPATCH");
@@ -44,14 +55,15 @@ contract BTCcontest is Ownable, ClientIntArray {
     address dispatchAddress = coordinator.getContract("DISPATCH");
     require(_id == query_id,"Query id is not correct");
     require(address(msg.sender)==address(dispatchAddress),"Only accept response from dispatch");
+    require(address(tx.or))
     require(contest.getStatus()==1,"Contest is not in initialized state"); //2 is the ReadyToSettle enum value
     uint256 price = uint256(responses[0]);
     bytes32[] memory endpoints = contest.getEndpoints();
     for(uint256 i=0;i<endpoints.length;i++){
-      if(endpoints[i]==upEndpoint && price > settlePrice){
+      if(endpoints[i]==upEndpoint && price > startPrice){
         return contest.judge(endpoints[i]);
       }
-      if(endpoints[i]==downEndpoint && price<settlePrice){
+      if(endpoints[i]==downEndpoint && price<startPrice){
         return contest.judge(endpoints[i]);
       }
     }
---
+++ test/7_BTCcontest_test.js
@@ -0,0 +1,157 @@
+import EVMRevert from './helpers/EVMRevert';
+
+const BigNumber = web3.BigNumber;
+
+const expect = require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .expect;
+
+const Utils = require('./helpers/utils');
+
+const ZapCoordinator = artifacts.require("ZapCoordinator");
+const Database = artifacts.require("Database");
+const Bondage = artifacts.require("Bondage");
+const Registry = artifacts.require("Registry");
+const ZapToken = artifacts.require("ZapToken");
+const Dispatch = artifacts.require("Dispatch");
+const Arbiter = artifacts.require("Arbiter");
+const Cost = artifacts.require("CurrentCost");
+const FactoryToken = artifacts.require("FactoryToken");
+const TokenFactory = artifacts.require("TokenFactory");
+const SampleContest = artifacts.require("SampleContest");
+const BTCcontest = artifacts.require("BTCcontest");
+
+contract('SampleContest', function (accounts) {
+    const owner = accounts[0];
+    const participant1 = accounts[1];
+    const participant2 = accounts[2];
+    const oracle = accounts[3];
+    const factoryOwner = accounts[4];
+    const coincap = accounts[5]
+
+    const publicKey = 111;
+    const marketPublicKey = 222;
+    const title = "test";
+    const marketTitle = "test_1";
+    const routeKeys = [1];
+    const params = ["param1", "param2"];
+
+    const specifier = "test-specifier";
+    const marketSpecifier = "test_spec_1";
+    const zeroAddress = Utils.ZeroAddress;
+
+    const piecewiseFunction = [3, 0, 1, 0, 100000000000000];
+    const broker = 0;
+
+    const tokensForOwner = new BigNumber("150000000e18");
+    const tokensForParts = new BigNumber("5000e18");
+    const approveTokens = new BigNumber("1000e18");
+    const dotBound = new BigNumber("999");
+
+    async function prepareTokens() {
+        await this.token.allocate(owner, tokensForOwner, { from: owner });
+        await this.token.allocate(participant1, tokensForSubscriber, { from: owner });
+        await this.token.allocate(participant2, tokensForSubscriber, { from: owner });
+        await this.token.approve(this.bondage.address, approveTokens, {from: participant1});
+        await this.token.approve(this.bondage.address, approveTokens, {from: participant2});
+    }
+
+    beforeEach(async function deployContracts() {
+        // Deploy initial contracts
+        this.currentTest.token = await ZapToken.new();
+        this.currentTest.coord = await ZapCoordinator.new();
+        const owner = await this.currentTest.coord.owner();
+        this.currentTest.db = await Database.new();
+        await this.currentTest.db.transferOwnership(this.currentTest.coord.address);
+        await this.currentTest.coord.addImmutableContract('DATABASE', this.currentTest.db.address);
+        await this.currentTest.coord.addImmutableContract('ZAP_TOKEN', this.currentTest.token.address);
+
+        // Deploy registry
+        this.currentTest.registry = await Registry.new(this.currentTest.coord.address);
+        await this.currentTest.coord.updateContract('REGISTRY', this.currentTest.registry.address);
+
+        // Deploy current cost
+        this.currentTest.cost = await Cost.new(this.currentTest.coord.address);
+        await this.currentTest.coord.updateContract('CURRENT_COST', this.currentTest.cost.address);
+
+        // Deploy Bondage
+        this.currentTest.bondage = await Bondage.new(this.currentTest.coord.address);
+        await this.currentTest.coord.updateContract('BONDAGE', this.currentTest.bondage.address);
+
+        // Hack for making arbiter an account we control for testing the escrow
+        // await this.currentTest.coord.addImmutableContract('ARBITER', accounts[3]);
+
+        await this.currentTest.coord.updateAllDependencies({ from: owner });
+
+        this.currentTest.tokenFactory = await TokenFactory.new();
+    });
+
+    function findEvent(logs, eventName) {
+        for (let i = 0; i < logs.length; i++) {
+            if (logs[i].event === eventName) {
+                return logs[i];
+            }
+        }
+
+        return null;
+    }
+
+    it("BTC CONTEST - lifecycle", async function () {
+        const startPrice = 8000
+        const upEndpoint = "BTC_UP"
+        const downEndpoint = "BTC_DOWN"
+        let factory = await SampleContest.new(this.test.coord.address, this.test.tokenFactory.address, publicKey, title);
+        let btcContest = await BTCcontest.new(factory.address, startPrice, upEndpoint, downEndpoint);
+        await this.test.registry.initiateProvider("coincap","coincap",{from:coincap});
+        await this.test.registry.initiateProviderCurve("coincap",piecewiseFunction,zeroAddress,{from:coincap})
+
+	let tx;//tmp var for event tracking
+
+	let ttl = 10;//10 blocks contest period
+
+	await factory.initializeCurve(upEndpoint,upEndpoint, piecewiseFunction);
+	await factory.initializeCurve(downEndpoint,downEndpoint, piecewiseFunction);
+        let reserveTokenAddr = await factory.reserveToken();
+        let reserveToken = await ZapToken.at(reserveTokenAddr);
+        await reserveToken.allocate(owner, tokensForOwner);
+        await reserveToken.allocate(participant1, tokensForOwner, {from: owner});
+        await reserveToken.allocate(participant2, tokensForOwner, {from: owner});
+        await reserveToken.approve(factory.address, tokensForOwner, {from: owner});
+        await reserveToken.approve(factory.address, tokensForOwner, {from: participant1});
+        await reserveToken.approve(factory.address, tokensForOwner, {from: participant2});
+        await btcContest.bondToCoincap(coincap,"coincap",5,{from:owner})
+	let zapBalance = await reserveToken.balanceOf(owner);
+	let zapAllowance = await reserveToken.allowance(owner, factory.address);
+	console.log('zap balance: ',zapBalance,', zap allowance: ',zapAllowance);
+	await factory.initializeContest(btcContest.address, ttl, {from: owner});
+
+//spec1 balance 1
+        let curveTokenAddr = await factory.getTokenAddress(upEndpoint);
+        let curveToken = await FactoryToken.at(curveTokenAddr);
+
+	await factory.bond(upEndpoint, 7, {from: participant1});
+	await factory.bond(downEndpoint, 8, {from: participant2});
+
+//spec1 balancce 2
+  let query_id = btcContest.queryToSettle(coincap,"coincap",{from:owner})
+  const tx = await dispatch.respond(query_id,[9000],{from:coincap})
+  //up endpoint should win
+	console.log('tx ', tx);
+
+	let winValue = await factory.winValue();
+	console.log('winValue: ', winValue.toNumber());
+
+
+// //spec1 balancce 2
+	upOwnerBalanceBefore = await curveToken.balanceOf(participant1);
+  downOwnerBalanceBefore = await curveToken.balanceOf(participant2);
+
+
+	console.log('participants balances: ', upOwnerBalance.toNumber(),downOwnerBalance.toNumber());
+	// let after = await reserveToken.balanceOf(owner);
+	// console.log('before: ',before.toString(),', after: ',after.toString());
+  //       let subBalance = parseInt(await reserveToken.balanceOf(owner));
+  //       await expect(subBalance).to.be.not.equal(10000)
+    });
+});
