0309ba797e97b43b735367f377f02ad1a44ad44e
7alexlin <7inalex@gmail.com>
2019-06-20 13:57:09 -0400

    Update Destructible.sol

--- contracts/lib/lifecycle/Destructible.sol
+++ contracts/lib/lifecycle/Destructible.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.5.0;
 
 import "../ownership/Ownable.sol";
 
+
+//was not compiling with selfdestruct(owner) added a require and changed parameter from owner to msg.sender
+
+
 contract Destructible is Ownable {
     function selfDestruct() public onlyOwner {
         require(owner == msg.sender);
